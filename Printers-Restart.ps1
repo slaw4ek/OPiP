72f2ab65f39ecceae5c635416ee01e4a79225f2b
slaw4ek <slawek256@gmail.com>
2018-03-13 15:04:01 +0100

    komputer marka
    wlaczenie spoolera 13.03.2018

---
+++ Printers-Restart.ps1
@@ -0,0 +1,25 @@
+﻿$cred = Get-Credential
+$comp= "d0p68c2"
+
+
+Invoke-Command -ComputerName $comp -Credential $cred {
+Stop-Service -Name Spooler
+}
+
+
+Invoke-Command -ComputerName $comp -Credential $cred {
+New-Item -ItemType Directory -Path C:\AdminTools\ -Name spool_bckp
+Move-Item -path C:\Windows\System32\spool\* -Destination C:\AdminTools\spool_bckp
+}
+
+
+Invoke-Command -ComputerName $comp -Credential $cred {
+New-Item -ItemType Directory -Path C:\AdminTools\ -Name spool_bckp
+Move-Item -path C:\AdminTools\spool_bckp\* -Destination C:\Windows\System32\spool
+}
+
+
+Invoke-Command -ComputerName $comp -Credential $cred {
+Start-Service -Name Spooler
+}
+
