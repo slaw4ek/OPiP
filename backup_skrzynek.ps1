d03e4063bb008683fdceb06bad7b7d5476bc7694
slaw4ek <slawek256@gmail.com>
2018-03-13 13:07:41 +0100

    zmiana
    backup skrzynki

---
+++ backup_skrzynek.ps1
@@ -0,0 +1,18 @@
+﻿$cred = Get-Credential -Message "Podaj login do usługi Office 365"
+
+$session = New-PSSession -ConfigurationName Microsoft.Exchange  -ConnectionUri "https://outlook.office365.com/powershell-liveid/"  -Credential $cred  -Authentication Basic  -AllowRedirection
+Import-PSSession $session
+
+Connect-MsolService -Credential $cred
+
+get-mailbox -SoftDeletedMailbox
+
+$guid1=(Get-Mailbox -SoftDeletedMailbox marek.szajda@zajezdnia.org).ExchangeGuid
+
+$guid2=(Get-Mailbox marek.szajda.bckp@zajezdnia.org).ExchangeGuid
+
+New-MailboxRestoreRequest -SourceMailbox $guid1.Guid -TargetMailbox $guid2.guid -AllowLegacyDNMismatch -verbose
+
+Get-MailboxRestoreRequest
+
+New-Mailbox -Shared -Name 
