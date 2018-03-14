f771d011ff9de4ec98c07b60f9b9082be77f15ea
slaw4ek <slawek256@gmail.com>
2018-03-14 13:43:02 +0100

    restart wszystkich spoolerów

---
+++ AllSpoolerRestart.ps1
@@ -0,0 +1,22 @@
+﻿$cred = Get-Credential
+
+$csv=Import-Csv -Delimiter ";" -Path C:\AdminTools\POSH\comps.csv
+
+Invoke-Command -ComputerName $csv.AssetName -Credential $cred {
+
+Stop-Service -Name Spooler
+
+sleep(5)
+
+New-Item -ItemType Directory -Path C:\AdminTools\ -Name spool_bckp
+
+Move-Item -path C:\Windows\System32\spool\* -Destination C:\AdminTools\spool_bckp
+
+sleep(10)
+
+Move-Item -path C:\AdminTools\spool_bckp\* -Destination C:\Windows\System32\spool
+
+sleep(5)
+
+Start-Service -Name Spooler
+}
