0625f862ada4068dcec2a943f0dca0168f9f4ed8
slaw4ek <slawek256@gmail.com>
2018-03-19 13:00:59 +0100

    Update Gpo
    Dla wszystkich komputerow

---
+++ GPOUpdateAll.ps1
@@ -0,0 +1,9 @@
+﻿$cred = Get-Credential
+
+$csv=Import-Csv -Delimiter ";" -Path C:\AdminTools\POSH\comps.csv
+
+Invoke-Command -ComputerName $csv.AssetName -Credential $cred -Verbose{
+
+gpupdate /force
+
+}
